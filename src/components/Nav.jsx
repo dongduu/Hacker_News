c80b3d39ab41db6527a4868c63fefb1627e2d920
Dongju-Lee <88874976+dongduu@users.noreply.github.com>
2022-01-29 22:16:49 +0900

    feat: delete css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,88 +1,91 @@
 import React, { useState } from "react";
 
-const navBoxStyle = {
-  position: "relative",
-  height: "40px",
-  width: "375px",
-  fontSize: "18px"
-};
-const initialStyle = {
-  display: "block",
-  width: "84px",
-  margin: "0 auto",
-  position: "absolute",
-  color: "#999",
-  padding: "auto",
-  top: "0",
-  bottom: "0",
-  borderBottom: "2px solid #F0F0F6"
-};
-const newsiInitialStyle = {
-  ...initialStyle,
-  left: "20px"
-};
-const showiInitialStyle = {
-  ...initialStyle,
-  left: "104px"
-};
-const askiInitialStyle = {
-  ...initialStyle,
-  right: "104px"
-};
-const jobsiInitialStyle = {
-  ...initialStyle,
-  right: "20px"
-};
+// const navBoxStyle = {
+//   position: "relative",
+//   height: "40px",
+//   width: "375px",
+//   fontSize: "18px"
+// };
+// const initialStyle = {
+//   display: "inline-block",
+//   width: "84px",
+//   height: "40px",
+//   position: "absolute",
+//   color: "#999",
+//   textAlign: "center",
+//   alignItems: "center",
+//   top: "0",
+//   borderBottom: "2px solid #F0F0F6"
+// };
+// const newsiInitialStyle = {
+//   ...initialStyle,
+//   left: "20px"
+// };
+// const showiInitialStyle = {
+//   ...initialStyle,
+//   left: "104px"
+// };
+// const askiInitialStyle = {
+//   ...initialStyle,
+//   right: "104px"
+// };
+// const jobsiInitialStyle = {
+//   ...initialStyle,
+//   right: "20px"
+// };
 
 const Nav = () => {
-  const [newsStyle, setNewsStyle] = useState(newsiInitialStyle);
-  const [showStyle, setShowStyle] = useState(showiInitialStyle);
-  const [askStyle, setAskStyle] = useState(askiInitialStyle);
-  const [jobsStyle, setJobsStyle] = useState(jobsiInitialStyle);
+  // const [newsStyle, setNewsStyle] = useState(newsiInitialStyle);
+  // const [newsClick, setNewsClick] = useState("default");
+  // const [showStyle, setShowStyle] = useState(showiInitialStyle);
+  // const [askStyle, setAskStyle] = useState(askiInitialStyle);
+  // const [jobsStyle, setJobsStyle] = useState(jobsiInitialStyle);
 
-  const onClickNews = (e) => {
-    setNewsStyle({
-      ...newsStyle,
-      color: "#FF6600",
-      borderBottom: "2px solid #FF6600"
-    });
-  };
-  const onClickShow = (e) => {
-    setShowStyle({
-      ...showStyle,
-      color: "#FF6600",
-      borderBottom: "2px solid #FF6600"
-    });
-  };
-  const onClickAsk = (e) => {
-    setAskStyle({
-      ...askStyle,
-      color: "#FF6600",
-      borderBottom: "2px solid #FF6600"
-    });
-  };
-  const onClickJobs = (e) => {
-    setJobsStyle({
-      ...jobsStyle,
-      color: "#FF6600",
-      borderBottom: "2px solid #FF6600"
-    });
-  };
+  // const onClickNews = (e) => {
+  //   if (e.target.props === undefined) {
+  //     console.log(e.target.props);
+  //     setNewsStyle({
+  //       ...newsStyle,
+  //       color: "#FF6600",
+  //       borderBottom: "2px solid #FF6600"
+  //     });
+  //     setNewsClick("clicked");
+  //   } else {
+  //     console.log(e.target.props);
+  //     setNewsStyle({
+  //       ...newsiInitialStyle
+  //     });
+  //     setNewsClick("default");
+  //   }
+  // };
+  // const onClickShow = (e) => {
+  //   setShowStyle({
+  //     ...showStyle,
+  //     color: "#FF6600",
+  //     borderBottom: "2px solid #FF6600"
+  //   });
+  // };
+  // const onClickAsk = (e) => {
+  //   setAskStyle({
+  //     ...askStyle,
+  //     color: "#FF6600",
+  //     borderBottom: "2px solid #FF6600"
+  //   });
+  // };
+  // const onClickJobs = (e) => {
+  //   setJobsStyle({
+  //     ...jobsStyle,
+  //     color: "#FF6600",
+  //     borderBottom: "2px solid #FF6600"
+  //   });
+  // };
 
   return (
-    <nav style={navBoxStyle}>
-      <a href="javascript:void(0)" style={newsStyle} onClick={onClickNews}>
-        News
-      </a>
-      <a href="javascript:void(0)" style={showStyle} onClick={onClickShow}>
-        Show
-      </a>
-      <a href="javascript:void(0)" style={askStyle} onClick={onClickAsk}>
-        Ask
-      </a>
-      <a href="javascript:void(0)" style={jobsStyle} onClick={onClickJobs}>
-        Jobs
-      </a>
+    <nav>
+      <a href="javascript:void(0)">News</a>
+      <a href="javascript:void(0)">Show</a>
+      <a href="javascript:void(0)">Ask</a>
+      <a href="javascript:void(0)">Jobs</a>
     </nav>
   );
 };
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,49 +1,57 @@
+import styled from "styled-components";
 // import React, { useState } from "react";
 
-const titleBoxStyle = {
-  height: "60px",
-  width: "375px",
-  border: "solid red 1px",
-  position: "relative"
-};
-const logoStyle = {
-  position: "absolute",
-  top: "16px",
-  left: "20px"
-};
-const pStyle = {
-  position: "absolute",
-  top: "14px",
-  left: "56px",
-  fontSize: "10px",
-  color: "#111"
-};
-const titleStyle = {
-  position: "absolute",
-  top: "24px",
-  left: "56px",
-  fontSize: "18px",
-  color: "#111"
-};
-const darkModeStyle = {
-  position: "absolute",
-  top: "23px",
-  right: "46px"
-};
-const qeustionStyle = {
-  position: "absolute",
-  top: "23px",
-  right: "22px"
-};
+styled.div`
+  height: 60px;
+  width: 375px;
+  border: solid red 1px;
+  position: relative;
+`;
+
+// const titleBoxStyle = {
+//   height: "60px",
+//   width: "375px",
+//   border: "solid red 1px",
+//   position: "relative"
+// };
+// const logoStyle = {
+//   position: "absolute",
+//   top: "16px",
+//   left: "20px"
+// };
+// const pStyle = {
+//   position: "absolute",
+//   top: "14px",
+//   left: "56px",
+//   fontSize: "10px",
+//   color: "#111"
+// };
+// const titleStyle = {
+//   position: "absolute",
+//   top: "24px",
+//   left: "56px",
+//   fontSize: "18px",
+//   color: "#111"
+// };
+// const darkModeStyle = {
+//   position: "absolute",
+//   top: "23px",
+//   right: "46px"
+// };
+// const qeustionStyle = {
+//   position: "absolute",
+//   top: "23px",
+//   right: "22px"
+// };
 
 const Title = () => {
   return (
-    <div style={titleBoxStyle}>
-      <img src="./images/logo.png" alt="logo" style={logoStyle} />
-      <p style={pStyle}>React</p>
-      <div style={titleStyle}>Hecker News</div>
-      <img src="./images/darkmode.png" alt="darkmode" style={darkModeStyle} />
-      <img src="./images/question.png" alt="question" style={qeustionStyle} />
+    <div>
+      <img src="./images/logo.png" alt="logo" />
+      <p>React</p>
+      <div>Hecker News</div>
+      <img src="./images/darkmode.png" alt="darkmode" />
+      <img src="./images/question.png" alt="question" />
     </div>
   );
 };
