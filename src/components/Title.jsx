7680e5939b087b873b5a22f4fb6ceefd7fdea79d
Dongju-Lee <88874976+dongduu@users.noreply.github.com>
2022-01-31 15:48:17 +0900

    feat: add title css

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,58 +1,60 @@
 import styled from "styled-components";
-// import React, { useState } from "react";
 
-styled.div`
+const TitleDiv = styled.div`
   height: 60px;
   width: 375px;
   border: solid red 1px;
   position: relative;
 `;
 
-// const titleBoxStyle = {
-//   height: "60px",
-//   width: "375px",
-//   border: "solid red 1px",
-//   position: "relative"
-// };
-// const logoStyle = {
-//   position: "absolute",
-//   top: "16px",
-//   left: "20px"
-// };
-// const pStyle = {
-//   position: "absolute",
-//   top: "14px",
-//   left: "56px",
-//   fontSize: "10px",
-//   color: "#111"
-// };
-// const titleStyle = {
-//   position: "absolute",
-//   top: "24px",
-//   left: "56px",
-//   fontSize: "18px",
-//   color: "#111"
-// };
-// const darkModeStyle = {
-//   position: "absolute",
-//   top: "23px",
-//   right: "46px"
-// };
-// const qeustionStyle = {
-//   position: "absolute",
-//   top: "23px",
-//   right: "22px"
-// };
+const Logo = styled.div`
+  position: absolute;
+  top: 17px;
+  left: 21px;
+`;
+
+const MainTitle = styled.div`
+  position: absolute;
+  top: 16px;
+  left: 60px;
+  font-size: 14px;
+  font-family: Roboto;
+  font-weight: 500;
+  font-style: italic;
+  line-height: 16px;
+  letter-spacing: 0.025em;
+  color: #505050;
+`;
+
+const DarkMode = styled.div`
+  position: absolute;
+  top: 19px;
+  right: 65px;
+`;
+
+const Help = styled.div`
+  position: absolute;
+  top: 20px;
+  right: 26px;
+`;
 
 const Title = () => {
   return (
-    <div>
-      <img src="./images/logo.png" alt="logo" />
-      <p>React</p>
-      <div>Hecker News</div>
-      <img src="./images/darkmode.png" alt="darkmode" />
-      <img src="./images/question.png" alt="question" />
-    </div>
+    <TitleDiv>
+      <Logo>
+        <img src={require("./images/logo.png")} alt="logo" />
+      </Logo>
+      <MainTitle>
+        <p>ReHecker</p>
+        <p>News</p>
+      </MainTitle>
+      <DarkMode>
+        <img src={require("./images/moon.png")} alt="darkmode" />
+      </DarkMode>
+      <Help>
+        <img src={require("./images/help.png")} alt="help" />
+      </Help>
+    </TitleDiv>
   );
 };
 
