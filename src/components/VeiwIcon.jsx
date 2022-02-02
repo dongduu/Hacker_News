0ba02b19af151ec9f4c651f7c752799c2150cd19
Dongju-Lee <88874976+dongduu@users.noreply.github.com>
2022-02-02 22:26:18 +0900

    feat: create VeiwMode component

---
+++ src/components/VeiwIcon.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const VeiwIcon = () => {
+  return (
+    <div>
+      <img src={require("./images/check.png")} alt="" />
+      <div>INI</div>
+    </div>
+  );
+};
+
+export default VeiwIcon;
