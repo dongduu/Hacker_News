d13dd3eee4e24741cc56d2a3c577243bd80be5af
Dongju-Lee <88874976+dongduu@users.noreply.github.com>
2022-02-03 19:22:46 +0900

    feat: create api file

--- src/components/HackerNews.jsx
+++ src/components/HackerNews.jsx
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 import View from "../components/View";
 import BasicCard from "../components/BasicCard";
-import DetailCard from "../components/DetailCard";
 
 const HackerNewsBox = styled.div`
   position: relative;
   width: 375px;
   padding-top: 8px;
-  border: 1px solid green;
 `;
 
 const HackerNews = () => {
@@ -15,7 +13,6 @@ const HackerNews = () => {
     <HackerNewsBox>
       <View />
       <BasicCard />
-      <DetailCard />
     </HackerNewsBox>
   );
 };
---
+++ src/services/api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export const BASE_URL = "https://hacker-news.firebaseio.com/v0";
+export const NEWSTORIES_URL = `${BASE_URL}/topstories.json`;
+export const ITEM_URL = `${BASE_URL}/item/`;
+
+export const getStories = async () => {
+  try {
+    const res = await axios.get(NEWSTORIES_URL).then(({ data }) => data);
+    return res;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getStory = async (storyId) => {
+  try {
+    const res = await axios
+      .get(`${ITEM_URL + storyId}.json`)
+      .then(({ data }) => data);
+    return res;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getComment = async (commentId) => {
+  try {
+    const res = await axios
+      .get(`${ITEM_URL + commentId}.json`)
+      .then(({ data }) => data);
+    return res;
+  } catch (err) {
+    console.error(err);
+  }
+};
