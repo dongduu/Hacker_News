998175f96391d1a257a13d0085b3d3647f2a1673
Dongju-Lee <88874976+dongduu@users.noreply.github.com>
2022-02-07 22:16:53 +0900

    feat: add conditional rendering

--- src/components/List/ShowList.js
+++ src/components/List/ShowList.js
@@ -1,7 +1,10 @@
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { BasicCard } from "../Card/BasicCard";
 import { DetailCard } from "../Card/DetailCard";
 
 export const ShowList = () => {
-  return <BasicCard />;
+  const [veiw, setView] = useState(true);
+
+  return <>{veiw ? <BasicCard /> : <DetailCard />}</>;
 };
