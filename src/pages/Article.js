bbf9845e7c73291343424bf55aaf76b2a4d8be6a
Dongju-Lee <88874976+dongduu@users.noreply.github.com>
2022-02-10 16:11:20 +0900

    feat: change route

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,25 +1,5 @@
-// import React, { useEffect, useState } from "react";
-// import { getTopStories } from "../services/api";
-// import { Title } from "../components/Header/Title";
-// import { Nav } from "../components/Header/Nav";
-// import { ArticleList } from "../components/List/ArticleList";
-// import { News } from "./News";
-
-// export const Article = () => {
-//   const [storyIds, setStoryIds] = useState([]);
-
-//   // useEffect(() => {
-//   //   getTopStories().then((ids) => setStoryIds(ids));
-//   // }, []);
-//   return (
-//     <div>
-//       <Title />
-//       <Nav />
-//       <ArticleList />
-//       {/* <News /> */}
-//     </div>
-//   );
-// };
+import { Title } from "../components/Header/Title";
+import { Nav } from "../components/Header/Nav";
 import React, { useState, useEffect } from "react";
 import BasicCard from "../components/Card/BasicCard";
 import { getNewIds } from "../services/api";
@@ -33,6 +13,8 @@ export const Article = () => {
 
   return (
     <>
+      <Title />
+      <Nav />
       <View />
       {newIds.slice(0, 10).map((id, index) => (
         <BasicCard id={id} key={id} index={id} />
